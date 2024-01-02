a46f15366a6d9b9aaa1d2e8a4decc2bbe3b04c25
Alissa2M <alissa2021k@gmail.com>
2024-01-02 20:34:10 +0900

    #1 都道府県一覧を取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,41 @@
-export default function Home() {
-  return <main>Hello, world!</main>;
+import { GetServerSideProps } from 'next';
+import { Prefecture } from './api/prefecture';
+
+interface HomeProps {
+  prefectures: Prefecture[];
 }
+
+export default function Home({ prefectures }: HomeProps) {
+  return (
+    <div>
+      <h1>都道府県一覧</h1>
+      <ul>
+        {prefectures.map((prefecture) => (
+          <li key={prefecture.prefCode}>{prefecture.prefName}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  try {
+    const response = await fetch('http://localhost:3002/api/prefecture'); // 本番環境では適切なURLに変更
+    if (response.ok) {
+      const prefectures = await response.json();
+      return {
+        props: {
+          prefectures,
+        },
+      };
+    }
+    throw new Error('Failed to fetch data');
+  } catch (error) {
+    console.error('Error during fetch:', error);
+    return {
+      props: {
+        prefectures: [],
+      },
+    };
+  }
+};
